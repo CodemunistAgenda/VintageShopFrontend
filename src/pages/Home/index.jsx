67d444d0c0c3000e3cfe0a7d6d9a4ab66ac2e89f
Orhan Güzel <orhan.guezel@dci-student.org>
2025-03-27 15:00:32 +0100

    structer ist ok

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../../styles/Home.css";
+
+const Home = () => {
+ 
+  return (
+    <div className="home-container">
+      <h1>
+        Willkommen bei <span className="highlight">Vintageshop</span> 
+      </h1>
+   
+    </div>
+  );
+};
+
+export default Home;
